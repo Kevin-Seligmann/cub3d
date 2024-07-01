594c7b8f8ea115008b9a0595e9fa963fe79fd6d3
kevin-seligmann <seligmann.kevin.cc@gmail.com>
2024-07-01 08:16:26 +0200

    Cubed

--- cub3d/libft/obj/ft_itoa.d
+++
@@ -1,2 +0,0 @@
-obj/ft_itoa.o: src/libft/ft_itoa.c inc/libft.h inc/ft_printf.h \
- inc/libft.h inc/get_next_line.h
--- cub3d/obj/parser_util.d
+++
@@ -1,3 +0,0 @@
-obj/parser_util.o: src/parser/parser_util.c inc/cubed.h libft/inc/libft.h \
- libft/inc/ft_printf.h libft/inc/libft.h libft/inc/get_next_line.h \
- MLX42/include/MLX42/MLX42.h inc/parser.h
