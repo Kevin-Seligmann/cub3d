0eb4d50c84125c180a54171e6a01cbd63b508c1d
kevin-seligmann <seligmann.kevin.cc@gmail.com>
2024-07-04 18:55:28 +0200

    Map parsing and MLX windows opening

--- libft/obj/ft_memcmp.d
+++
@@ -1,2 +0,0 @@
-obj/ft_memcmp.o: src/libft/ft_memcmp.c inc/libft.h inc/ft_printf.h \
- inc/libft.h inc/get_next_line.h
--- libft/obj/ft_putchar_fd.d
+++
@@ -1,2 +0,0 @@
-obj/ft_putchar_fd.o: src/libft/ft_putchar_fd.c inc/libft.h \
- inc/ft_printf.h inc/libft.h inc/get_next_line.h
