c81b65817a4e35e37941dfdb58d1f2afbd3a7d97
kevin-seligmann <seligmann.kevin.cc@gmail.com>
2024-07-30 20:33:14 +0200

    Make fclean

--- obj/is_map_line.d
+++
@@ -1,3 +0,0 @@
-obj/is_map_line.o: src/parser/is_map_line.c inc/cubed.h libft/inc/libft.h \
- libft/inc/ft_printf.h libft/inc/libft.h libft/inc/get_next_line.h \
- MLX42/include/MLX42/MLX42.h inc/cubed_data_structures.h inc/parser.h
